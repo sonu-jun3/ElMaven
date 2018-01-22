6695f2f6862f47eae32d00376eab1e896a76ebf2
Deepak Sharma <deepak.sharma@elucidata.io>
2018-01-22 18:24:52 +0530

    [ADD]: Updated run.sh file

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 OMP_CANCELLATION=true
-export QT_SELECT=qt5 
+export QT_SELECT=qt5
 flag=100   #this flag check wether qt running in debug mode or not
 while true; do
     read -p "Do you wish to install this program in release mode? " yn
@@ -9,16 +9,8 @@ while true; do
         * ) echo "Please answer yes or no.";;
     esac
 done
+make -j $(nproc)
 
-if [ "$(uname)" == "Darwin" ]; then
-      make -j $(sysctl -n hw.ncpu)
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    make -j $(nproc)
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    make -j $(nproc)
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    make -j $(nproc)
-fi
 
 if [ -f tests/MavenTests/test.xml ]; then
 	rm test*.xml
@@ -28,12 +20,9 @@ if [ -f ./bin/MavenTests ]; then
 	./bin/MavenTests -xml
 fi
 
-type="$(uname)" 
+type="$(uname)"
 systemType="$(echo "$type"  |  tr '[:upper:]'  '[:lower:]')"
 if [ $systemType == "linux" ] && [ $flag == 10 ]; then
     lcov --capture --directory ./ --output-file coverage.info
     genhtml coverage.info --output-directory coverage
 fi
-
-cd tests/featureTesting/
-python main.py
