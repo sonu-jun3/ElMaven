662d0d6f70304eab0d2fed09b2838845040662a3
Shubhra Agrawal <shubhra.agrawal@elucidata.io>
2018-05-21 01:27:06 +0530

    [UI] Add checkbox to show/hide parent label in plot #529
    Adding UI element for show/hide parent feature in the isotope plot dock widget
    
    Issue: #529

--- src/gui/mzroll/isotopeplotdockwidget.cpp
+++ src/gui/mzroll/isotopeplotdockwidget.cpp
@@ -32,29 +32,37 @@ void IsotopePlotDockWidget::setToolBar()
     spacer1->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     toolBar->addWidget(spacer1);
 
-    QCheckBox *C13 = new QCheckBox("C13");
-    C13->setChecked(true);
-    toolBar->addWidget(C13);
+    QCheckBox *C12 = new QCheckBox("C12 Parent");
+    C12->setChecked(true);
+    toolBar->addWidget(C12);
 
     QWidget *spacer2 = new QWidget();
     spacer2->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     toolBar->addWidget(spacer2);
 
-    QCheckBox *N15 = new QCheckBox("N15");
-    N15->setChecked(false);
-    toolBar->addWidget(N15);
+    QCheckBox *C13 = new QCheckBox("C13");
+    C13->setChecked(true);
+    toolBar->addWidget(C13);
 
     QWidget *spacer3 = new QWidget();
     spacer3->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     toolBar->addWidget(spacer3);
 
-    QCheckBox *D2 = new QCheckBox("D2");
-    D2->setChecked(false);
-    toolBar->addWidget(D2);
+    QCheckBox *N15 = new QCheckBox("N15");
+    N15->setChecked(false);
+    toolBar->addWidget(N15);
 
     QWidget *spacer4 = new QWidget();
     spacer4->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     toolBar->addWidget(spacer4);
+
+    QCheckBox *D2 = new QCheckBox("D2");
+    D2->setChecked(false);
+    toolBar->addWidget(D2);
+
+    QWidget *spacer5 = new QWidget();
+    spacer5->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    toolBar->addWidget(spacer5);
 
     QCheckBox *S34 = new QCheckBox("S34");
     S34->setChecked(false);
